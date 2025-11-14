46d8b3c6f4897b2b75cc5f512c01bbfd3c7c9bbd
hengdiliang <38797718+hengdiliang@users.noreply.github.com>
2025-11-14 12:36:38 -0800

    Update model includes
    Commented out old includes for nutrient and carbon models and added new includes for NPZBD and SUPRA models.

--- src/ClimaOceanBiogeochemistry.jl
+++ src/ClimaOceanBiogeochemistry.jl
@@ -4,7 +4,9 @@ module ClimaOceanBiogeochemistry
 include("CarbonSystemSolvers.jl")
 
 # diffferent kinds of BGC model
-include("nutrients_plankton_bacteria_detritus.jl")
-include("carbon_alkalinity_nutrients.jl")
+# include("nutrients_plankton_bacteria_detritus.jl")
+# include("carbon_alkalinity_nutrients.jl")
+include("NPZBD.jl")
+include("SUPRA.jl")
 
 end # module ClimaOceanBiogeochemistry
